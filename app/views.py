4df87fad60a1a663802d4a9a8ad7c0900c7a3aea
Taha Sanawad <85823152+Taha-Sanawad-wala@users.noreply.github.com>
2023-04-15 16:06:52 +0530

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from app.models import Github
+from app.models import Github,ContactMe
 # Create your views here.
 def index(request):
     d=Github.objects.all()
-    for i in d:
-        print(i.image.url)
-    data={"data":d}
-    return render(request,"index.html",data)
+    if request.method=="POST":
+        if request.POST.get('name')!="" and request.POST.get('email')!="" and request.POST.get('number')!="" and request.POST.get('message')!="": 
+            cm=ContactMe(name=request.POST.get('name'),email=request.POST.get('email'),number=request.POST.get('number'),message=request.POST.get('message'))
+            cm.save()
+            data={"data":d}
+            return render(request,"index.html",data)
+        else:
+            data={"data":d}
+            return render(request,"index.html",data)
+
+    else:
+        data={"data":d}
+        return render(request,"index.html",data)
